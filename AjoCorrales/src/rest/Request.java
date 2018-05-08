503b3e0e2dbce5ce49cd90e9c773837a9f4dbc53
acorrales <alvarocorrales92@gmail.com>
2018-05-08 21:03:45 +0200

    Properties. Eliminado DataSource. Prueba Connection

--- AjoCorrales/src/rest/Request.java
+++ AjoCorrales/src/rest/Request.java
@@ -1,56 +1,54 @@
 package rest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.apache.naming.NamingContext;
+import org.apache.log4j.Logger;
 
 import javassist.bytecode.stackmap.TypeData.ClassName;
-import org.apache.log4j.Logger;
 
 @Path("upmsocial")
 public class Request {
 	@Context
 	private UriInfo uriInfo;
 
-	private DataSource ds;
 	private Connection conn;
-	
+
 	private static final Logger log = Logger.getLogger(ClassName.class.getName());
-	
+
 	public Request() {
 		log.info("Dentro del constructor");
-		InitialContext ctx;
+		Properties prop = new Properties();
+		InputStream input = Request.class.getClassLoader().getResourceAsStream("bbdd.properties");
 		try {
-			ctx = new InitialContext();
-			NamingContext envCtx = (NamingContext) ctx.lookup("java:comp/env");
-
-			ds = (DataSource) envCtx.lookup("jdbc/UPMSocial");
-			
-			conn = ds.getConnection();
-		} catch (NamingException | SQLException e) {
+			prop.load(input);
+			Class.forName(prop.getProperty("driver"));
+			conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"),
+					prop.getProperty("passwd"));
+		} catch (ClassNotFoundException | SQLException | IOException e) {
 			log.error(e.getMessage() + e.getStackTrace());
 		}
 	}
 
 	// Lista de garajes JSON/XML generada con listas en JAXB
 	@GET
-	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Response getUsuarios() {
+		log.info("Petición recibida en getUsuarios()");
 		PreparedStatement ps = null;
 		Request g = new Request();
 		try {
@@ -63,14 +61,15 @@ public class Request {
 				log.info(rs.getString(2));
 			}
 			rs.close();
-			return Response.status(Response.Status.OK).entity(g).build(); // No se puede devolver el ArrayList (para generar XML)
+			return Response.status(Response.Status.OK).entity("OK").build(); // No se puede devolver el ArrayList (para
+																				// generar XML)
 		} catch (NumberFormatException e) {
 			return Response.status(Response.Status.BAD_REQUEST).entity("No se pudieron convertir los índices a números")
 					.build();
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error de acceso a BBDD").build();
-		}finally {
-			if(ps != null)
+		} finally {
+			if (ps != null)
 				try {
 					ps.close();
 				} catch (SQLException e) {
@@ -78,6 +77,5 @@ public class Request {
 				}
 		}
 	}
-	
-	
+
 }
