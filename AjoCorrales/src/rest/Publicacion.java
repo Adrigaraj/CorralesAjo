9c8d353e9d325b4180a3945016cb98069a334ada
restuser <restuser@UbuntuVM>
2018-05-16 15:23:07 +0200

    Clase publicacion

---
+++ AjoCorrales/src/rest/Publicacion.java
@@ -0,0 +1,67 @@
+package rest;
+
+import java.sql.Date;
+
+import org.json.JSONObject;
+
+public class Publicacion {
+	private String idPublicacion;
+	private Date fechaPublicacion;
+	private String propietario;
+	private String tweet;
+
+	public Publicacion(String idPublicacion, Date fechaPublicacion, String propietario, String tweet) {
+		this.idPublicacion = idPublicacion;
+		this.fechaPublicacion = fechaPublicacion;
+		this.propietario = propietario;
+		this.tweet = tweet;
+	}
+
+	public JSONObject toJSON() {
+		JSONObject JsObj = new JSONObject();
+		JsObj.put("idPublicacion", this.idPublicacion);
+		JsObj.put("fechaPublicacion", this.fechaPublicacion);
+		JsObj.put("propietario", this.propietario);
+		JsObj.put("tweet", this.tweet);
+		return JsObj;
+	}
+
+	public String getIdPublicacion() {
+		return idPublicacion;
+	}
+
+	public void setIdPublicacion(String idPublicacion) {
+		this.idPublicacion = idPublicacion;
+	}
+
+	public Date getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+
+	public void setFechaPublicacion(Date fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+
+	public String getPropietario() {
+		return propietario;
+	}
+
+	public void setPropietario(String propietario) {
+		this.propietario = propietario;
+	}
+
+	public String getTweet() {
+		return tweet;
+	}
+
+	public void setTweet(String tweet) {
+		this.tweet = tweet;
+	}
+
+	@Override
+	public String toString() {
+		return "Publicacion [idPublicacion=" + idPublicacion + ", fechaPublicacion=" + fechaPublicacion
+				+ ", propietario=" + propietario + ", tweet=" + tweet + "]";
+	}
+
+}
